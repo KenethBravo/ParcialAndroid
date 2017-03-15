99415828e83cff9ba4512db4530d3b76e5f7bd1d
aula7 <pc716@gmail.com>
2017-03-15 13:48:12 -0500

    Conexion de vitas

--- app/src/main/java/com/example/aula7/parcial/View_Contact.java
+++ app/src/main/java/com/example/aula7/parcial/View_Contact.java
@@ -2,6 +2,7 @@ package com.example.aula7.parcial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class View_Contact extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class View_Contact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view__contact);
+        showToolbar(getResources().getString(R.string.txt_principal_menu),true);
+    }
+
+    private void showToolbar(String title, boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
